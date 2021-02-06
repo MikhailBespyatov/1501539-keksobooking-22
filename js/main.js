cd1fe476622df057e8f86e2cdc1c4779113b5b1e
Misha <besp9tov.mikhail94@gmail.com>
2021-02-06 22:34:40 +0500

    проработал замечание

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const NUMBERS_ADS_ARRAY = 10;
+const OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const OFFER_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+const OFFER_TYPE = ['palace', 'flat', 'house', 'bungalow'];
+const OFFER_TIME = ['12:00', '13:00', '14:00'];
+
 const getRandomNumber = (min = 0, max = 1) => {
   if (min >= 0 && max >= 0) {
     return (min >= max)
@@ -18,25 +28,17 @@ const getRandomOfferItem = (array) => {
   return array[getRandomNumber(0, array.length - 1)];
 };
 
+const randomOfferArray = (array) => {
+  return new Array(getRandomNumber(1, array.length)).fill(null).map(() => getRandomOfferItem(array));
+};
+
 const unique = (array) => {
   return Array.from(new Set(array));
 };
 
-const offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-const offerPhotos = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
-
-const randomOfferFeatures =
-new Array(getRandomNumber(1, 10)).fill(null).map(() => getRandomOfferItem(offerFeatures));
-
-const uniqueOfferFeatures = unique(randomOfferFeatures);
-
-const randomOfferPhotos =
-new Array(getRandomNumber(1, 10)).fill(null).map(() => getRandomOfferItem(offerPhotos));
+const uniqueOfferArray = (array) => {
+  return unique(randomOfferArray(array));
+};
 
 const createAd = () => {
   return {
@@ -48,26 +50,25 @@ const createAd = () => {
       title: 'Заголовок объявления',
       address: `${getRandomFractionalNumber(0, 100, 2)}, ${getRandomFractionalNumber(0, 100, 2)}`,
       price: `${getRandomNumber(100, 10000)}`,
-      type: 'palace',
+      type: `${getRandomOfferItem(OFFER_TYPE)}`,
       rooms: `${getRandomNumber(1, 5)}`,
       guests: `${getRandomNumber(2, 10)}`,
-      checkin: '14:00',
-      checkout: '12:00',
-      features: uniqueOfferFeatures,
+      checkin: `${getRandomOfferItem(OFFER_TIME)}`,
+      checkout: `${getRandomOfferItem(OFFER_TIME)}`,
+      features: uniqueOfferArray(OFFER_FEATURES),
       description: 'Очень уютный номер с видом на перламутровый залив',
-      photos: randomOfferPhotos,
-      location: {
-        x: getRandomFractionalNumber(35.65000, 35.70000, 5),
-        y: getRandomFractionalNumber(139.70000, 139.80000, 5),
-      },
+      photos: uniqueOfferArray(OFFER_PHOTOS),
+    },
+
+    location: {
+      x: getRandomFractionalNumber(35.65000, 35.70000, 5),
+      y: getRandomFractionalNumber(139.70000, 139.80000, 5),
     },
   }
 }
 
-const NUMBERS_ADS_ARRAY = 10;
-
-const createAds = () => {
-  return new Array(NUMBERS_ADS_ARRAY).fill(null).map(() => createAd());
+const createAds = (number) => {
+  return new Array(number).fill(null).map(() => createAd());
 };
 
-createAds();
+createAds(NUMBERS_ADS_ARRAY);
